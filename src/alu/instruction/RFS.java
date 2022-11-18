f35f8668c1ca845c13bfd6e538bb3fca72be56c4
Ketan Kapse <ketanckapse@gmail.com>
2022-11-18 18:09:16 -0500

    Add files via upload

---
+++ src/alu/instruction/RFS.java
@@ -0,0 +1,29 @@
+package alu.instruction;
+
+import cpu.Registers;
+import memory.MCU;
+//import util.EffectiveAddress;
+import util.MachineFaultException;
+import util.StringUtil;
+
+public class RFS extends AbstractInstruction {
+
+    int immed;
+
+    @Override
+    public void execute(String instruction, Registers registers, MCU mcu) throws MachineFaultException {
+        // -----------------------------------
+        // 015: RFS -> Return From Subroutine w/ return code 
+        // -----------------------------------
+        immed = StringUtil.binaryToDecimal(instruction.substring(11, 16));
+        registers.setR0(immed);
+        registers.setPC(registers.getR3());
+    }
+
+    @Override
+    public String getExecuteMessage() {
+        // TODO Auto-generated method stub
+        return "RFS " + immed;
+    }
+
+}
